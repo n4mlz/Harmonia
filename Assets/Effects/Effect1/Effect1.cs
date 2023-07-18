c27a9259ec254a078661813e7b305087ac0dddb2
Nameless-itf23 <ohutoncat@gmail.com>
2023-07-18 19:50:38 +0900

    big big big update Assets

---
+++ Assets/Effects/Effect1/Effect1.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.EventSystems;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using MidiJack;
+
+public class Effect1 : MonoBehaviour
+{
+    int key;
+    private AsyncOperationHandle<GameObject> handle;
+
+    // Start is called before the first frame update
+    async void Start()
+    {
+        key = GetComponentInParent<Key>().key;
+        handle = Addressables.LoadAssetAsync<GameObject>("Effect1/Cube1.prefab");  // インスタンス化するプレハブ
+        await handle.Task;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Vector3 getSpawnPoint()
+    {
+        return new Vector3((key-64)*0.3f, 0, 0);
+    }
+
+
+    private void NoteOn(MidiChannel channel, int note, float velocity) {
+        if (note == key){
+            // 処理を記述
+            Vector3 spawnPoint = getSpawnPoint();
+            GameObject instance = Instantiate(handle.Result, spawnPoint, Quaternion.identity);
+        }
+   }
+
+   private void NoteOff(MidiChannel channel, int note) {
+        if (note == key){
+            // 処理を記述
+        }
+   }
+}
--- Assets/Effects/Effect2/Cube2.cs
+++ Assets/Effects/Effect2/Cube2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cube : MonoBehaviour
+public class Cube2 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
