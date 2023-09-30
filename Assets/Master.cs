35fc91141d797354f94106deee6b2ba5dde92b6c
Nola <rei11a2004@gmail.com>
2023-09-30 18:29:42 +0900

    setup Effect3 for test

--- Assets/Master.cs
+++ Assets/Master.cs
@@ -8,11 +8,9 @@ public class Master : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
         GameObject effectObject = new GameObject($"effect");
         effectObject.transform.SetParent(transform);
-        effectObject.AddComponent<Effect3>();  // ここでエフェクトを指定
-        
+        effectObject.AddComponent<test_Effect3>();  // ここでエフェクトを指定
     }
 
     // Update is called once per frame
