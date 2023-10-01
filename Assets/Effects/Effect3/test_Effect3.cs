526da1a983c48e4dd6857e66d2098ccc8009c705
Nola <130660018+NolaTA3141592@users.noreply.github.com>
2023-10-01 18:33:56 +0900

    Add Effect3

---
+++ Assets/Effects/Effect3/test_Effect3.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MidiJack;
+
+public class test_Effect3 : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        for (int i=21; i<109; i++)  // note 21 ~ 108
+        {
+            GameObject keyObject = new GameObject($"Key{i}");
+            keyObject.transform.SetParent(transform);
+            keyObject.AddComponent<test_Key3>();
+            keyObject.GetComponent<test_Key3>().key = i;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.J)) {
+            test_NoteOn(50, 1.0f);
+        }
+        if(Input.GetKeyUp(KeyCode.J)) {
+            test_NoteOff(50, 1.0f);
+        }
+        
+    }
+
+    void test_NoteOn(int note, float velocity) {
+        GameObject child = transform.Find($"Key{note}").gameObject;
+        child.GetComponent<test_Key3>().On(note, velocity);
+    }
+
+    void test_NoteOff(int note, float velocity) {
+        GameObject child = transform.Find($"Key{note}").gameObject;
+        child.GetComponent<test_Key3>().Off(note);
+    }
+    // void OnEnable() {
+    //     MidiMaster.noteOnDelegate += NoteOn;
+    //     MidiMaster.noteOffDelegate += NoteOff;
+    // }
+
+    // void OnDisable() {
+    //     MidiMaster.noteOnDelegate -= NoteOn;
+    //     MidiMaster.noteOffDelegate -= NoteOff;
+    // }
+
+    // void NoteOn(MidiChannel channel, int note, float velocity) {
+    //     GameObject child = transform.Find($"Key{note}").gameObject;
+    //     child.GetComponent<Key3>().On(channel, velocity);
+    // }
+
+    // void NoteOff(MidiChannel channel, int note) {
+    //     GameObject child = transform.Find($"Key{note}").gameObject;
+    //     child.GetComponent<Key3>().Off(channel);
+    // }
+}
---
+++ Assets/Effects/Effect3/test_Key3.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.EventSystems;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using MidiJack;
+
+public class test_Key3 : MonoBehaviour
+{
+    public int key;
+    private AsyncOperationHandle<GameObject> handle;
+
+    private GameObject[] particles = new GameObject[150];
+    
+    // Start is called before the first frame update
+    async void Start()
+    {
+        transform.position = getSpawnPoint();
+        // handle = Addressables.LoadAssetAsync<GameObject>("Effect3/Cube3.prefab");  // インスタンス化するプレハブ
+        handle = Addressables.LoadAssetAsync<GameObject>("Effect3/Particle.prefab");  // インスタンス化するプレハブ
+        await handle.Task;
+
+        particles[key] = Instantiate(handle.Result, transform.position, Quaternion.identity);
+        particles[key].transform.SetParent(transform);
+        particles[key].GetComponent<Particle>().off();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Vector3 getSpawnPoint()
+    {
+        return new Vector3((key-64)*0.3f, -4, 0);
+    }
+
+    public void On(int note, float velocity) {
+        // if(particles[note] != null) {
+        //     Debug.Log($"key {note} is not null");
+        // }
+        // GameObject instance = Instantiate(handle.Result, transform.position, Quaternion.identity);
+        // instance.transform.SetParent(transform);
+        // particles[note] = instance;
+        particles[note].GetComponent<Particle>().on();
+    }
+
+    public void Off(int note) {
+        // if(particles[note] == null) {
+        //     Debug.Log($"key {note} is null");
+        // }
+        particles[note].GetComponent<Particle>().off();
+    }
+}
