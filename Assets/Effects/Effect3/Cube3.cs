9d7bee8f83eb0ae0e797346c724dd982b2a1584e
Nola <rei11a2004@gmail.com>
2023-10-02 12:05:35 +0900

    update ParticleLine

--- Assets/Effects/Effect3/Cube3.cs
+++ Assets/Effects/Effect3/Cube3.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Cube3 : MonoBehaviour
 {
     private int shift = 0;
-    public float speed = 0.01f;
+    public float speed = 0.015f;
     // Start is called before the first frame update
     void Start()
     {
